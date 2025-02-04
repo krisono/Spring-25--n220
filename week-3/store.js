18b7e3158b7d65c9e0caf87c769571af056aec75
Nnaemeka <kristopheronochie@gmail.com>
2025-02-04 17:28:25 -0500

    update to code after canvas comment

--- week-3/store.js
+++ week-3/store.js
@@ -1,10 +1,10 @@
 let price = 40.99// i used let because this number could possibly be dynamic if i decalre it again
 const sales_tax = 0.07;
-console.log("Item Price: $", price); // I used const because this value is nopt to be changed
-console.log("Total After Tax: ", price * (1 + sales_tax));
-
-let newprice = 60.99 // i used let because this number could possibly be dynamic if i decalre it again
+console.log("Item Price: $", price); 
+const priceaftertax = price * (1 + sales_tax); 
+console.log("Total After Tax: ", priceaftertax);
+const validdiscount = price > 50; // I used const because this value is nopt to be changed
 const discount_rate = 0.10 // const because this is NOT a dynamic discount rate but rather a firm one. 
-console.log("Discounted Total After Tax: ", newprice * (1 + sales_tax) * (1 - discount_rate));
+console.log("Discounted Total After Tax: ", priceaftertax * (1 - discount_rate * (validdiscount)));
 
 
