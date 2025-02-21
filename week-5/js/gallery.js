89c86702adc4e901469b11dd883dfc9ec14cff2e
Nnaemeka <kristopheronochie@gmail.com>
2025-02-20 22:10:29 -0500

    more

---
+++ week-5/js/gallery.js
@@ -0,0 +1,21 @@
+/*const googleLinkRef = document.querySelector('https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI', 
+    'https://fastly.picsum.photos/id/185/300/400.jpg?hmac=7jvc9SGhArOCJhgLoOfYSTezJsc1xrIXhDOXkiZSrbs', 
+    'https://fastly.picsum.photos/id/229/300/400.jpg?hmac=CZOWn8sQ8oa9EfbMZw5tYqPMNKK0mNOYUsEgz81K8io', 
+    'https://fastly.picsum.photos/id/211/300/400.jpg?hmac=l0zi1AkBxaPoAMnWav0iWH8TsWfblHXdm_IGSNDMFWQ'
+);*/
+
+const googleLinkRef = ['https://fastly.picsum.photos/id/804/300/400.jpg?hmac=Y6cx9oentnw75-Q7gYdlaJBBAnSAlUHRYXbvFMIrZNI', 
+    'https://fastly.picsum.photos/id/185/300/400.jpg?hmac=7jvc9SGhArOCJhgLoOfYSTezJsc1xrIXhDOXkiZSrbs', 
+    'https://fastly.picsum.photos/id/229/300/400.jpg?hmac=CZOWn8sQ8oa9EfbMZw5tYqPMNKK0mNOYUsEgz81K8io', 
+    'https://fastly.picsum.photos/id/211/300/400.jpg?hmac=l0zi1AkBxaPoAMnWav0iWH8TsWfblHXdm_IGSNDMFWQ'
+];
+const googleLink = document.querySelector('gallery');
+console.log(googleLink);
+
+for (let i = 0; i < googleLinkRef.length; i++) {
+    const img = document.createElement('img');
+    img.setAttribute('src', googleLinkRef[i])
+    img.setAttribute('alt', 'Image #' + (i + 1)) 
+    img.height = 300;
+    gallery.appendChild(img);
+}
---
+++ week-5/readme.md
@@ -0,0 +1,10 @@
+# Week 5
+## Assignment 4
+
+### Weekly recap
+*Hello evryone its **ME***,
+I learnt about how to make equations
+I also learnt how to properly call a ul tag from the html to the javascript file
+### Live link
+
+[Home Page](https://krisono.github.io/Spring-25--n220/week-5/index.html)
